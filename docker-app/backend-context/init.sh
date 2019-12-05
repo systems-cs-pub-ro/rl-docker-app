a1b7a453b44dc8d458778d7afb5b90e1bb04a1e8
Octavian <octavian-grigorescu>
2019-12-05 19:22:34 +0200

    Move pip3 packages installation in Dockerfile

--- docker-app/backend-context/init.sh
+++ docker-app/backend-context/init.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-pip3 install mysqlclient
-pip3 install flask
-pip3 install flask_migrate
-pip3 install flask-sqlalchemy
-pip3 install mysql-connector-python
-pip3 install sqlalchemy
-
 flask db init
 flask db migrate
 flask db upgrade
