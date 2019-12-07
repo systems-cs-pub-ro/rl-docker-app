2835c8d86063aa0303e438e6814101f037f2de53
Stancu Florin <niflostancu@gmail.com>
2019-12-07 16:18:31 +0200

    backend: fix mysql race condition & images copy issue

--- docker-app/backend-context/init.sh
+++ docker-app/backend-context/init.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# wait for database to become ready
+echo "Waiting for mysql to start..."
+while ! mysqladmin ping -h"$DB_SERVER" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
+    sleep 1
+done
+
 flask db migrate
 flask db upgrade
 
 python3 init_db.py
 
 python3 run.py
+
