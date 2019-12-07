31402ff29f23f87cb77d04034797a81eb2c2e4b2
Stancu Florin <niflostancu@gmail.com>
2019-12-07 16:18:53 +0200

    fix nginx site configuration issue
    (envsubst race condition)

--- docker-app/frontend-context/init-frontend.sh
+++ docker-app/frontend-context/init-frontend.sh
@@ -3,8 +3,10 @@
 export FRONTEND_NODE
 export BACKEND_SERVER
 
-envsubst '${FRONTEND_NODE} ${BACKEND_SERVER}' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default
+envsubst '${FRONTEND_NODE} ${BACKEND_SERVER}' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default.tmp
+mv -f /etc/nginx/sites-available/default.tmp /etc/nginx/sites-available/default
 
 mkdir -p /etc/nginx/sites-enabled/
+ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 exec "$@"
