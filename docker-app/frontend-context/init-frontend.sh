3f1680d33512fc8931e3d2fd47b45389cebe8277
Stancu Florin <niflostancu@gmail.com>
2019-12-07 19:59:04 +0200

    frontend: fix nginx backend DNS race condition

--- docker-app/frontend-context/init-frontend.sh
+++ docker-app/frontend-context/init-frontend.sh
@@ -9,4 +9,5 @@ mv -f /etc/nginx/sites-available/default.tmp /etc/nginx/sites-available/default
 mkdir -p /etc/nginx/sites-enabled/
 ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
+echo "Starting nginx..."
 exec "$@"
